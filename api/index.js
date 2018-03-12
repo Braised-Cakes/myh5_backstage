cf6df08423c906846ef3ecf77f0787e94b761862
Braised-Cakes <1930003003@qq.com>
2018-03-12 19:28:48 +0800

    add aj/list/del && adj/list/add

--- api/index.js
+++ api/index.js
@@ -9,12 +9,9 @@ const {
 } = require('../const/index')
 app.get('/aj/list/get', async (req, res) => {
     const myh5 = dbHandel.getModel('myh5')
-
     const data = await getDataSync(myh5, {
-        field : ['work_id']
+        field: ['work_id']
     })
-    console.log(data);
-
     res.send({
         status: 1,
         message: '删除失败',
@@ -26,5 +23,35 @@ app.get('/aj/list/get', async (req, res) => {
             },
             data: data
         }
+    })
+});
+
+
+app.get('/aj/list/add', async (req, res) => {
+    const myh5 = dbHandel.getModel('myh5')
+    const count = await getCountSync(myh5, {})
+    new myh5({
+        work_id: count + 1,
+        username: '9999'
+    }).save((err, docs) => {
+        if (err) throw err
+        res.send({
+            msg: '创建成功'
+        })
+    })
+});
+
+/**
+ * 列表页， 删除
+ */
+app.get('/aj/list/del', async (req, res) => {
+    const myh5 = dbHandel.getModel('myh5')
+    myh5.remove({
+        work_id: req.query.work_id
+    }, () => {
+        res.send({
+            status: 1,
+            msg: '删除成功'
+        });
     })
 });
